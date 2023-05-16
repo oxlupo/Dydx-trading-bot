d33b444b0cf807195f902d4a865ed650c842751b
yousef <mr.ysaj@gmail.com>
2023-05-16 10:07:45 +0330

    [ADD-FILE] add program folder in git

---
+++ program/main.py
@@ -0,0 +1,9 @@
+from dydx3 import Client
+from web3 import Web3
+from pprint import pprint
+from datetime import datetime, timedelta
+
+from dydx3.constants import API_HOST_GOERLI
+
+if __name__ == "__main__":
+    print("hello World")
