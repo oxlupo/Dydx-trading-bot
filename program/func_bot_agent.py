be74637a890ce4f9630d0ac5c7ab8079c4389ba4
yousef <mr.ysaj@gmail.com>
2023-05-21 10:42:23 +0330

    [UPDATE] class was updated and check m1 DONE

--- program/func_bot_agent.py
+++ program/func_bot_agent.py
@@ -18,7 +18,7 @@ class BotAgent:
                 market_2,
                 base_side,
                 base_size,
-                best_price,
+                base_price,
                 quote_side,
                 quote_size,
                 quote_price,
@@ -33,7 +33,7 @@ class BotAgent:
         self.market_2 = market_2
         self.base_side = base_side
         self.base_size = base_size
-        self.best_price = best_price
+        self.base_price = base_price
         self.quote_side = quote_side
         self.quote_size = quote_size
         self.quote_price = quote_price
@@ -93,4 +93,69 @@ class BotAgent:
         return "live"
 
     # Open trades
+    def open_trades(self):
+        # Print status
+        print("---")
+        print(f"{self.market_1}: placing first order...")
+        print(f"Side: {self.base_side}\nSize: {self.base_size}\nPrice:{self.base_price}")
+        print("---")
 
+        # Place Base Order
+        try:
+            base_order = place_market_order(
+                self.client,
+                market=self.market_1,
+                side=self.base_side,
+                size=self.base_size,
+                price=self.base_price,
+                reduce_only=False,
+            )
+            # Store the order id
+            self.order_dict["order_id_m1"] = base_order["order"]["id"]
+            self.order_dict["order_time_m1"] = datetime.now().isoformat()
+        except Exception as e:
+            self.order_dict["pair_status"] = "ERROr"
+            self.order_dict["comments"] = f"Market 1 {self.market_1}: {e}"
+            return self.order_dict
+
+        # Ensure order is live before processing
+        order_status_m1 = self.check_order_status_by_id(self.order_dict["order_id_m1"])
+
+        # Guard: Aborder if order failed
+        if order_status_m1 != "live":
+            self.order_dict["pair_status"] = "ERROR"
+            self.order_dict["comments"] = f"{self.market_1}: failed to fill"
+            return self.order_dict
+
+        # Print status - opening second order
+        print("---")
+        print(f"{self.market_2}: placing first order...")
+        print(f"Side: {self.quote_side}\nSize: {self.quote_size}\nPrice:{self.quote_price}")
+        print("---")
+
+        # Place Base Order
+        try:
+            quote_order = place_market_order(
+                self.client,
+                market=self.market_1,
+                side=self.quote_side,
+                size=self.quote_size,
+                price=self.quote_price,
+                reduce_only=False,
+            )
+            # Store the order id
+            self.order_dict["order_id_m2"] = quote_order["order"]["id"]
+            self.order_dict["order_time_m2"] = datetime.now().isoformat()
+        except Exception as e:
+            self.order_dict["pair_status"] = "ERROr"
+            self.order_dict["comments"] = f"Market 2 {self.market_2}: {e}"
+            return self.order_dict
+
+        # Ensure order is live before processing
+        order_status_m2 = self.check_order_status_by_id(self.order_dict["order_id_m2"])
+
+        # Guard: Aborder if order failed
+        if order_status_m1 != "live":
+            self.order_dict["pair_status"] = "ERROR"
+            self.order_dict["comments"] = f"{self.market_1}: failed to fill"
+            return self.order_dict
